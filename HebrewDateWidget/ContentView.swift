5fedf0851264a118f609681dc8a8127050c84136
livelybride <livelybride@users.noreply.github.com>
2026-02-02 09:36:20 +0800

    Initial Commit

---
+++ HebrewDateWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HebrewDateWidget
+//
+//  Created by Michelle Yeow on 02/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HebrewDateWidget/HebrewDateWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HebrewDateWidgetApp.swift
+//  HebrewDateWidget
+//
+//  Created by Michelle Yeow on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HebrewDateWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
